cd6b463355d86807fbc5ea183fe5b8a4de8e05ce
Egor1198 <belosokhovegor@gmail.com>
2024-07-29 18:49:17 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+task = 12
+course = 'Python'
+hour = 1.5
+time = (hour/task)
+
+
+print('Курс: ' + course +','+'всего задач:'+ str(task)+','+'затрачено часв:'+str(hour)+','+'среднее время выполнения '+str(time)+' часа.')
